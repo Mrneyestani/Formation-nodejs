25f1f1ddd0cf55d17c64ec810410cbccc17dc055
Reza <mrneyestani@gmail.com>
2023-02-27 13:59:46 +0100

    Mettre en place mon premier projet nodejs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Formation NodeJS
+
+Dans cette formation vous apprenderz à créer des api web en utilisant nodejs et mongo db.
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("coucou");
+console.log("coucou");
+console.log("coucou");
+console.log("coucou");
+console.log("coucou");
