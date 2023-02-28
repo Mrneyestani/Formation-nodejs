d3665eead9f10dca9c29d94a01895b2721307549
Reza <mrneyestani@gmail.com>
2023-02-28 14:08:00 +0100

    add env

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-console.log("Coucou");
-console.log("Salut ça marche");
+console.log(process.env.NODE_ENV);
+console.log(process.env.TZ);
+console.log(process.env.HOST);
+console.log(process.env.PORT);
