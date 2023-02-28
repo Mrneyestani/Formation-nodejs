715f9b0ae0d3e8157c30a7bb23629bd9d3ab10c0
Reza <mrneyestani@gmail.com>
2023-02-28 16:55:58 +0100

    add fastify

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-console.log(process.env.NODE_ENV);
-console.log(process.env.TZ);
-console.log(process.env.HOST);
-console.log(process.env.PORT);
+import fastify from "fastify";
+
+console.log(`NODE_ENV = ${process.env.NODE_ENV}`);
+console.log(`TZ = ${process.env.TZ}`);
+console.log(`HOST = ${process.env.HOST}`);
+console.log(`PORT = ${process.env.PORT}`);
+
+const app = fastify();
+
+app.listen({ port: process.env.PORT as any, host: process.env.HOST }, () => {
+  console.log(
+    `Le serveur http est prêt sur l'address : http://${process.env.HOST}:${process.env.PORT}"`
+  );
+});
+
+app.get("/", () => {
+  return "Bienvenue sur mon serveur";
+});
+
+app.get("/hello", () => {
+  return "Bonjour tout le monde";
+});
